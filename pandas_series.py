c2f3cca082d21ecff26bdda921e7aad4ece77947
Jarred Cole <jarred.aj.cole@gmail.com>
2023-06-01 06:27:15 -0500

    update

--- pandas_series.py
+++ pandas_series.py
@@ -190,3 +190,112 @@ upper_series
 letters.value_counts().head(6).plot(kind="barh")
 
 
+# Use pandas to create a Series named numbers from the following list:
+numbers = ['$796,459.41', '$278.60', '$482,571.67', '$4,503,915.98', '$2,121,418.3', '$1,260,813.3', '$87,231.01', '$1,509,175.45', '$4,138,548.00', '$2,848,913.80', '$594,715.39', '$4,789,988.17', '$4,513,644.5', '$3,191,059.97', '$1,758,712.24', '$4,338,283.54', '$4,738,303.38', '$2,791,759.67', '$769,681.94', '$452,650.23']
+numbers = pd.Series(numbers)
+numbers
+
+# What is the data type of the numbers Series?
+numbers.dtype
+# or
+numbers.describe()
+# object
+
+
+# How many elements are in the number Series?
+len(numbers)
+# or
+numbers.describe()
+# 20 elements
+
+
+# Perform the necessary manipulations by accessing Series attributes and methods to convert the numbers 
+# Series to a numeric data type.
+numbers = numbers.str.replace('$','')
+numbers = numbers.str.replace(',','')
+numbers = numbers.astype(float)
+
+
+# Run the code to discover the maximum value from the Series.
+numbers.max()
+
+
+# Run the code to discover the minimum value from the Series.
+numbers.min()
+
+
+# What is the range of the values in the Series?
+range = numbers.max() - numbers.min()
+range
+
+# Bin the data into 4 equally sized intervals or bins and output how many values fall 
+# into each bin.
+pd.qcut(numbers, 4).value_counts()
+# 5 values per bin. 20/4 = 5
+
+
+# Plot the binned data in a meaningful way. Be sure to include a title and axis labels.
+import matplotlib.pyplot as plt
+
+pd.cut(numbers,4).value_counts().sort_index().plot(kind='barh')
+plt.title('binned data')
+plt.xlabel('count')
+plt.ylabel('$ bins')
+plt.show()
+
+
+# Use pandas to create a Series named exam_scores from the following list:
+
+grades = [60, 86, 75, 62, 93, 71, 60, 83, 95, 78, 65, 72, 69, 81, 96, 80, 85, 92, 82, 78]
+grades = pd.Series(grades)
+grades
+
+# How many elements are in the exam_scores Series?
+grades.describe()
+
+
+# Run the code to discover the minimum, the maximum, the mean, and the median scores for 
+# the exam_scores Series.
+grades.describe()
+
+
+# Plot the Series in a meaningful way and make sure your chart has a title and axis labels.
+pd.cut(grades,4).value_counts().plot(kind="barh")
+plt.title('binned data')
+plt.xlabel('count')
+plt.ylabel('$ bins')
+plt.show()
+# or
+grades.plot.hist()
+plt.title('Grade Distribution')
+plt.xlabel('Grades')
+plt.ylabel('Percentage of Occurance')
+plt.show()
+
+
+# Write the code necessary to implement a curve for your exam_grades Series and save this as 
+# curved_grades. Add the necessary points to the highest grade to make it 100, and add the same 
+# number of points to every other score in the Series as well.
+curve_amount = 100 - grades.max()
+curved_g = grades + curve_amount
+curved_g
+
+
+# Use a method to convert each of the numeric values in the curved_grades Series into a 
+# categorical value of letter grades. For example, 86 should be a 'B' and 95 should be an 'A'. 
+# Save this as a Series named letter_grades.
+bin_z = [0, 70, 75, 80, 90, 101]
+bin_l = ['F', 'D', 'C', 'B','A']
+
+letter_grades = pd.cut(curved_g, bins=bin_z, labels=bin_l, right = False)
+letter_grades
+letter_grades = letter_grades.value_counts()
+
+
+
+# Plot your new categorical letter_grades Series in a meaninful way and include a title and axis labels.
+letter_grades.sort_values().plot.barh()
+plt.title('Letter Grade Count')
+plt.xlabel('Number of Grade Occurance')
+plt.ylabel('Letter Grade')
+plt.show()
